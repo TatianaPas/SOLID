40fbd78773728cbbb8bcd73a4f981474e8af15c2
Tatiana <tatjana.pasechnik@gmail.com>
2023-11-18 11:05:21 +1300

    Leave allocation repositry

--- HRLeaveManagementClean/HR.LeaveManagement.Persistence/Repositories/LeaveAllocationRepository.cs
+++ HRLeaveManagementClean/HR.LeaveManagement.Persistence/Repositories/LeaveAllocationRepository.cs
@@ -1,6 +1,7 @@
 ﻿using HR.LeaveManagement.Persistence.DatabaseContext;
 using HRLeaveManagementApplication.Contracts.Persistance;
 using HRLeaveManagementDomain;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,6 +14,48 @@ namespace HR.LeaveManagement.Persistence.Repositories
     {
         public LeaveAllocationRepository(HrDatabaseContext context) : base(context)
         {
+        }
+
+        public async Task AddAllocations(List<LeaveAllocation> allocations)
+        {
+            await _context.AddRangeAsync(allocations);  
+        }
+
+        public async Task<bool> AllocationExists(string userId, int leaveTypeId, int period)
+        {
+            return await _context.LeaveAllocations.AnyAsync(q => q.EmployeeId == userId
+            && q.LeaveTypeId == -leaveTypeId && q.Period == period);
+        }
+
+        public async Task<List<LeaveAllocation>> GetLeaveAllocationsWithDetails()
+        {
+            var leaveAllocations = await _context.LeaveAllocations
+                .Include(q=>q.LeaveType)
+                .ToListAsync();
+            return leaveAllocations;    
+        }
+
+        public async Task<List<LeaveAllocation>> GetLeaveAllocationsWithDetails(string userId)
+        {
+            var leaveAllocations = await _context.LeaveAllocations
+                .Where(q=>q.EmployeeId==userId)
+                .Include(q => q.LeaveType)
+                .ToListAsync();
+            return leaveAllocations;
+        }
+
+        public async Task<LeaveAllocation> GetLeaveAllocationWithDetails(int id)
+        {
+            var leaveAllocations = await _context.LeaveAllocations
+                .Include(q => q.LeaveType)
+                .FirstOrDefaultAsync(q=>q.Id==id);
+            return leaveAllocations;
+        }
+
+        public async Task<LeaveAllocation> GetUserAllocations(string userId, int leaveTypeId)
+        {
+            return await _context.LeaveAllocations.FirstOrDefaultAsync(q => q.EmployeeId==userId
+            && q.LeaveTypeId==leaveTypeId);
         }
     }
 }
--- HRLeaveManagementClean/HR.LeaveManagement.Persistence/Repositories/LeaveRequestRepository.cs
+++ HRLeaveManagementClean/HR.LeaveManagement.Persistence/Repositories/LeaveRequestRepository.cs
@@ -19,21 +19,25 @@ namespace HR.LeaveManagement.Persistence.Repositories
         public async Task<List<LeaveRequest>> GetLeaveRequestsWithDetails()
         {
             var leaveRequests = await _context.LeaveRequests
-                .Include(q=>q.LeaveType).ToListAsync();
+                .Include(q=>q.LeaveType)
+                .ToListAsync();
             return leaveRequests;
         }
 
         public async Task<List<LeaveRequest>> GetLeaveRequestsWithDetails(string userId)
         {
-            var leaveRequests = await _context.LeaveRequests.Where(q=>q.RequestingEmployeeId==userId)
-                .Include(q => q.LeaveType).ToListAsync();
+            var leaveRequests = await _context.LeaveRequests
+                .Where(q=>q.RequestingEmployeeId==userId)
+                .Include(q => q.LeaveType)
+                .ToListAsync();
             return leaveRequests;
         }
 
         public async Task<LeaveRequest> GetLeaveRequestWithDetails(int id)
         {
             var leaveRequest = await _context.LeaveRequests
-                .Include(q => q.LeaveType).FirstOrDefaultAsync(q=>q.Id==id);
+                .Include(q => q.LeaveType)
+                .FirstOrDefaultAsync(q=>q.Id==id);
             return leaveRequest;
         }
     }
